1cfac70d040d7a89707e313da7b6d0cd81379069
符寿金 <fushoujin@xjj.com>
2022-01-17 16:02:38 +0800

    完善廉政网首页

--- lzw/v2/js/index.js
+++ lzw/v2/js/index.js
@@ -1,5 +1,50 @@
-const initSwiper = options => new Swiper('.news-rotaion', options)
-const newsSwiper = initSwiper({
-  autoplay: true,
-  loop: true
-})
+const initSwiper = (options, className) => new Swiper(className, options)
+const newsSwiper = initSwiper(
+  {
+    autoplay: {
+      disableOnInteraction: false
+    },
+    loop: true
+  },
+  '.news-rotaion'
+)
+
+const khnSwiper_one = initSwiper(
+  {
+    autoplay: {
+      disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+      nextEl: '#khn-next-btn',
+      prevEl: '#khn-previous-btn'
+    }
+  },
+  '.khn-swiper-0'
+)
+const khnSwiper_two = initSwiper(
+  {
+    autoplay: {
+      disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+      nextEl: '#khn-next-btn',
+      prevEl: '#khn-previous-btn'
+    }
+  },
+  '.khn-swiper-1'
+)
+const khnSwiper_three = initSwiper(
+  {
+    autoplay: {
+      disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+      nextEl: '#khn-next-btn',
+      prevEl: '#khn-previous-btn'
+    }
+  },
+  '.khn-swiper-2'
+)
