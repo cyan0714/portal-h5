c3cc3566be3fa5218618d4c391c09a1bb70c4ff2
叶云峰 <yeyf@xjj.com>
2022-01-19 19:43:18 +0800

    样式分割

--- lzw/v2/js/index.js
+++ lzw/v2/js/index.js
@@ -1,54 +1,43 @@
-const initSwiper = (options, className) => new Swiper(className, options)
-const newsSwiper = initSwiper(
-  {
+var  newsSwiper = new Swiper(".news-rotaion", {
     autoplay: {
-      disableOnInteraction: false
+        disableOnInteraction: false
     },
     loop: true
-  },
-  '.news-rotaion'
-)
+})
 
-const khnSwiper_one = initSwiper(
-  {
-    autoplay: {
-      disableOnInteraction: false
-    },
-    loop: true,
-    navigation: {
-      nextEl: '#khn-next-btn',
-      prevEl: '#khn-previous-btn'
-    }
-  },
-  '.khn-swiper-0'
-)
-const khnSwiper_two = initSwiper(
-  {
-    autoplay: {
-      disableOnInteraction: false
-    },
-    loop: true,
-    navigation: {
-      nextEl: '#khn-next-btn',
-      prevEl: '#khn-previous-btn'
-    }
-  },
-  '.khn-swiper-1'
-)
-const khnSwiper_three = initSwiper(
-  {
-    autoplay: {
-      disableOnInteraction: false
-    },
-    loop: true,
-    navigation: {
-      nextEl: '#khn-next-btn',
-      prevEl: '#khn-previous-btn'
-    }
-  },
-  '.khn-swiper-2'
-)
 
+var  khnSwiper_one = new Swiper(".khn-swiper-0", {
+    autoplay: {
+        disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+        nextEl: '#khn-next-btn',
+        prevEl: '#khn-previous-btn'
+    }
+})
+
+var  khnSwiper_two = new Swiper(".khn-swiper-1", {
+    autoplay: {
+        disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+        nextEl: '#khn-next-btn',
+        prevEl: '#khn-previous-btn'
+    }
+})
+
+var  khnSwiper_three = new Swiper(".khn-swiper-2", {
+    autoplay: {
+        disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+        nextEl: '#khn-next-btn',
+        prevEl: '#khn-previous-btn'
+    }
+})
 layui.use('element', function () {
   var element = layui.element
   
