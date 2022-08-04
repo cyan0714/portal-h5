cbb46f67879be86e64e6c792f4ccc14c3617130f
hongyu <731347546@qq.com>
2022-08-04 17:18:27 +0800

    企业页面开发

--- gwymh/dataShow/js/gwy_portal.js
+++ gwymh/dataShow/js/gwy_portal.js
@@ -6,9 +6,25 @@ function changeTabNav(index) {
     let tabNavList = $('.protal-content-tabnav-item');
     tabNavList.removeClass('protal-content-tabnav-item-active');
     $(tabNavList[index]).addClass('protal-content-tabnav-item-active');
+    let protalBarList = $('.protal-bar');
+    protalBarList.removeClass('protal-bar-show');
+    $(protalBarList[index]).addClass('protal-bar-show');
+    layui.use('laydate', function () {
+        var laydate = layui.laydate;
+        //年选择器
+        laydate.render({
+            elem: `#yearSelect${index}`,
+            type: 'year',
+            value: 2020
+        });
+    });
 }
 
-function changeCensusDetailsTabs(e, index) {
+function changeCensusDetailsTabs(e, index, type) {
     $('.census-details-tabs-item').removeClass('census-details-tabs-item-active')
     $(e.target).parent().addClass('census-details-tabs-item-active')
+    let censusList = $(`.${type}-bar .census-details-content-item`);
+    censusList.removeClass('census-details-content-item-active');
+    $(censusList[index]).addClass('census-details-content-item-active');
+    console.log(censusList)
 }
